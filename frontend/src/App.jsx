99aa1bafd84db5628c69de6f7dd7a2c9d796e397
Srinivas Senapati <srinivassenapati5624@gmail.com>
2026-04-29 23:14:52 +0530

    register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./features/auth/pages/Login";
-// import Register from "./features/auth/pages/Register";
+import Register from "./features/auth/pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
